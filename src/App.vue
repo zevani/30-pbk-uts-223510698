c8f3b49a571b7214240c6e069da9df43101a9f1f
zevani <sy.zevanianandazellery@student.uir.ac.id>
2024-05-28 19:55:48 +0700

    perubahan ke 2 kali

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>
-    <label for="selection">Pilih Opsi:</label>
-    <select id="selection" v-model="selectedOption">
-      <option v-for="option in options" :key="option" :value="option">{{ option }}</option>
-    </select>
-    <p>Opsi terpilih: {{ selectedOption }}</p>
+  <div>    
+    <div>
+      <button
+        v-for="option in options"
+        :key="option"
+        :value="option"
+        @click="selectOption(option)"
+        :class="{'active': selectedOption === option}"
+      >
+        {{ option }}
+      </button>
+    </div>
+    <p> {{ selectedOption }}</p>
     <component :is="currentComponent"></component>
   </div>
 </template>
@@ -12,28 +19,30 @@
 <script setup>
 import { ref, computed } from 'vue'
 
-
-import post from './components/post.vue'
-import todo from './components/todo.vue'
+import Post from './components/post.vue'
+import Todo from './components/todo.vue'
 
 const options = ["Postingan", "Todo list"]
-const selectedOption = ref("Postingan") 
-
+const selectedOption = ref("Postingan")
 
 const currentComponent = computed(() => {
   if (selectedOption.value === "Postingan") {
-    return post
+    return Post
   } else if (selectedOption.value === "Todo list") {
-    return todo
+    return Todo
   }
   return null
 })
+
+const selectOption = (option) => {
+  selectedOption.value = option
+}
 </script>
 
 <style scoped>
 div {
   font-family: Arial, sans-serif;
-  margin: 200px;
+  margin: 20px;
 }
 
 label {
@@ -42,12 +51,19 @@ label {
   font-weight: bold;
 }
 
-select {
-  padding: 5px;
-  margin-bottom: 20px;
+button {
+  padding: 10px 15px;
+  margin-right: 10px;
   font-size: 16px;
   border: 1px solid #ccc;
   border-radius: 4px;
+  cursor: pointer;
+  background-color: #f8f9fa;
+}
+
+button.active {
+  background-color: #7900FF;
+  color: rgb(12, 12, 12);
 }
 
 p {
@@ -56,4 +72,3 @@ p {
   color: #333;
 }
 </style>
-
